f27b3fcfff3cba2301e032d53f4812767fc9dd6c
Your Name <you@example.com>
2024-06-04 21:22:24 +0100

    premiere sauvegarder

---
+++ tailwind.js
@@ -0,0 +1,37 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'gray-50': '#F1F1F1',
+                'gray-100': '#FAFAFA',
+                'gray-200': '#E0E0E0',
+                'gray-800': '#686868',
+                'red-500': '#FF5151'
+            },
+
+            fontFamily: {
+                'body': ["Poppins", "sans-serif"],
+            },    
+
+            padding: {
+                '18': '70px',
+            },
+
+            fontSize: {
+                '5.5px': '55px',
+            },
+
+            gap: {
+                '18': '70px',
+            },
+
+            boxShadow: {
+                'text-2d': '0px 4px 4px 0px #00000040',
+            },
+        }, 
+
+
+    }, 
+
+    plugins: [],
+  }
